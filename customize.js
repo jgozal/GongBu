95243c8347af1cf20fd95ff8593348414d2f9275
Jimmy Gozal <jgozal@developerkit.co>
2015-10-03 21:50:45 -0400

    whack

---
+++ customize.js
@@ -0,0 +1,46 @@
+jQuery(document).ready(function($) {
+    
+    var text = "";
+    
+    var user = { 
+        strong: 'bb',
+        italics: 'ii',
+        green:'gg'
+    };
+
+    var html = {
+        strong: "<strong>",
+        italics: "<i>",
+        green: "<p style= 'color:green'>"
+    }
+
+
+    var regex = null;
+
+    $('#editor').keyup(function(){translateContent()});
+
+        function translateContent () {
+        text =  $('#editor').val();
+        
+        for (key in user) {
+    regex = new RegExp(user[key], 'g');
+    text = text.replace(regex, html[key]);
+    $('#translator').html(text);;
+    };
+
+
+    };
+
+    
+});
+
+
+/*  for (key in user) {
+    regex = /bb([^)]+)bb/gmi;
+    text = text.replace(regex, html[key]+ text.match(regex) + html[key]);
+    $('#translator').html(text);
+    };
+
+    */
+
+
